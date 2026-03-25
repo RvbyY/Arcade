d5ffb58c3c4e722f99a5a7ae6383af0408d6652e
bassirou <bassirou.tall@epitech.eu>
2026-03-25 15:22:48 +0100

    feat: split test in one per file

---
+++ tests/test_parsing_2.cpp
@@ -0,0 +1,48 @@
+/* ------------------------------------------------------------------------------------ *
+ *                                                                                      *
+ * EPITECH PROJECT - Thu, Mar, 2026                                                     *
+ * Title           - G-OOP-400-STG-4-1-arcade-5                                         *
+ * Description     -                                                                    *
+ *     test_parsing                                                                     *
+ *                                                                                      *
+ * ------------------------------------------------------------------------------------ *
+ *                                                                                      *
+ *             ███████╗██████╗ ██╗████████╗███████╗ ██████╗██╗  ██╗                     *
+ *             ██╔════╝██╔══██╗██║╚══██╔══╝██╔════╝██╔════╝██║  ██║                     *
+ *             █████╗  ██████╔╝██║   ██║   █████╗  ██║     ███████║                     *
+ *             ██╔══╝  ██╔═══╝ ██║   ██║   ██╔══╝  ██║     ██╔══██║                     *
+ *             ███████╗██║     ██║   ██║   ███████╗╚██████╗██║  ██║                     *
+ *             ╚══════╝╚═╝     ╚═╝   ╚═╝   ╚══════╝ ╚═════╝╚═╝  ╚═╝                     *
+ *                                                                                      *
+ * ------------------------------------------------------------------------------------ */
+
+#include <iostream>
+#include <cassert>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fstream>
+
+bool isExist(std::string lib)
+{
+
+    struct stat buffer;
+
+    return (stat (lib.c_str(), &buffer) == 0);
+}
+
+bool parsing(std::string lib)
+{
+    if (isExist(lib) == false)
+        return false;
+    if (lib.substr(lib.find_last_of(".") + 1) != "so")
+        return false;
+    return true;
+}
+
+int main(void)
+{
+    assert(parsing("") == false);
+
+    return 0;
+}
