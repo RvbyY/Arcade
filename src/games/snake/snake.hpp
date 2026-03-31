d9b5bea01f5a53f95013d66eb80445ea385d6792
AdryzR <adrien.raphanaud@epitech.eu>
2026-03-31 17:04:07 +0200

    feat: update gitignore

---
+++ src/games/snake/snake.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2026
+** Arcade
+** File description:
+** snake
+*/
+
+#pragma once
+
+#include "lib/libarcade/Arcade/game.hpp"
+
+namespace Arcade {
+    class IDisplay;
+    struct Player;
+
+    class Snake : public IGame {
+        IGame() = default;
+        
+    }
+}
